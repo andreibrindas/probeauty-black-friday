a431e5f1a82df1590056d6801e43798a85001799
andreibrindas <31765195+andreibrindas@users.noreply.github.com>
2020-11-11 17:39:00 +0200

    updates

---
+++ src/pages/thank-you.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+// import Layout from '../components/layout';
+import Hero from "../components/hero"
+
+export default function Home() {
+  return (
+    <div>
+      <Hero></Hero>
+    </div>
+  )
+}
